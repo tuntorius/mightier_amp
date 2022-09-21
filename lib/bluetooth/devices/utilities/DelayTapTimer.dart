75a82b2e638886ee054d2cca1d0afa7ace046d7c
tuntorius <dian@gameline.co>
2022-09-21 22:41:18 +0300

    vastly improved tap tempo

--- lib/bluetooth/devices/utilities/DelayTapTimer.dart
+++ lib/bluetooth/devices/utilities/DelayTapTimer.dart
@@ -1,28 +1,28 @@
 class DelayTapTimer {
   final timeout = 1500;
+  final maxSamples = 20;
   List<DateTime> timeArray = <DateTime>[];
 
   addClickTime() {
-    timeArray.add(DateTime.now());
-    while (timeArray.length > 3) timeArray.removeAt(0);
+    var now = DateTime.now();
+
+    if (timeArray.isNotEmpty &&
+        now.difference(timeArray.last).inMilliseconds > timeout)
+      timeArray.clear();
+
+    timeArray.add(now);
+
+    while (timeArray.length > maxSamples) timeArray.removeAt(0);
   }
 
   calculate() {
     if (timeArray.length < 2) return false;
-    var length = timeArray.length;
-    var current = timeArray[length - 1].millisecondsSinceEpoch;
-    var last = timeArray[length - 2].millisecondsSinceEpoch;
-    //check for timeout and clear if it is
 
-    if (length > 2 && (current - last >= this.timeout)) {
-      while (timeArray.length > 1) timeArray.removeAt(0);
+    int sum = 0;
+    //get the sum of all differences and calculate average
+    for (int i = 0; i < timeArray.length - 1; i++)
+      sum += timeArray[i + 1].difference(timeArray[i]).inMilliseconds;
 
-      return false;
-    }
-
-    if (length == 2) return current - last;
-
-    var first = timeArray[length - 3].millisecondsSinceEpoch;
-    return (current - first) / 2;
+    return sum / (timeArray.length - 1);
   }
 }
